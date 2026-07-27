7bbf44ba6341f49f83b755d43f2f12a7dd0b3c0a
kaka alifha reza <kakaalifacms@gmail.com>
2026-07-27 11:34:00 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log('Hello World!');
+// 1. Impor module yang diperlukan dari firebase dan firestore
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js"
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    query,
+    orderBy,
+    onSnapshot,
+    serverTimestamp, 
+    dic, 
+    updateDoc, 
+    incrememt
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC_1XwbW-9zcNkRJXIQr4N1GgAyzQr6O2g",
+    authDomain: "uasgenap2026-549f7.firebaseapp.com",
+    projectId: "uasgenap2026-549f7",
+    storageBucket: "uasgenap2026-549f7.firebasestorage.app",
+    messagingSenderId: "560108074909",
+    appId: "1:560108074909:web:50c7719780334c88bee174",
+    measurementId: "G-E2WG6980GN"
+};
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const medsosCollection = collection(db, "medsos")
